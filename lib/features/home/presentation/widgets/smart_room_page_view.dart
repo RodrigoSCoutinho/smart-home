8b9ad3a333df579b8530eca88f0168f47799420d
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-01-29 15:20:01 -0300

    feat: adding the parallax

--- lib/features/home/presentation/widgets/smart_room_page_view.dart
+++ lib/features/home/presentation/widgets/smart_room_page_view.dart
@@ -7,38 +7,46 @@ import '../../../smart_room/screens/room_details_screen.dart';
 class SmartRoomsPageView extends StatelessWidget {
   const SmartRoomsPageView({
     super.key,
-    required this.controller,
+    required this.controller, 
+    required this.pageNotifier,
   });
 
   final PageController controller;
+  final ValueNotifier<double> pageNotifier;
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: controller,
-      clipBehavior: Clip.none,
-      itemCount: SmartRoom.fakeValues.length,
-      itemBuilder: (_, index) {
-        final room = SmartRoom.fakeValues[index];
-        return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: RoomCard(
-            percent: 0,
-            expand: false,
-            room: room,
-            onSwipeUp: () {},
-            onSwipeDown: () {},
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => RoomDetailScreen(room: room),
-                ),
-              );
-            },
-          ),
+    return ValueListenableBuilder(
+      valueListenable: pageNotifier,
+      builder: (_, page, __) {
+        return PageView.builder(
+          controller: controller,
+          clipBehavior: Clip.none,
+          itemCount: SmartRoom.fakeValues.length,
+          itemBuilder: (_, index) {
+            final room = SmartRoom.fakeValues[index];
+            double percent = page - index;
+            return Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: RoomCard(
+                percent: percent,
+                expand: false,
+                room: room,
+                onSwipeUp: () {},
+                onSwipeDown: () {},
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RoomDetailScreen(room: room),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
         );
-      },
+      }
     );
   }
 }
